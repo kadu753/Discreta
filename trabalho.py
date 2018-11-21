7c6d50f710c31cc64be60e8ec120e9642704a0cf
Kadu Grando <kadugrando@iMac-de-Kadu.local>
2018-11-21 18:56:33 -0200

    Iniciar

---
+++ trabalho.py
@@ -0,0 +1,7 @@
+# def recorrenciaA():
+# 	for i in range(3, 12):
+
+
+dia = input('Qual é o dia do seu aniversário?\n')
+mes = input('Qual é a unidade do mês do seu aniversário\n')
+ano = input('Qual é o algarismo das unidades do ano do seu aniversário\n')
